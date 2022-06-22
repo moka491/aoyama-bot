7a6f0c9ecb66e5568093f6d70da9c02bb696b8b1
Jannis Becker <jbecker@jannisbecker.me>
2022-06-22 09:56:46 +0200

    fix: clean up thread ping logic

--- src/commands/thread.rs
+++ src/commands/thread.rs
@@ -9,35 +9,38 @@ use poise::serenity_prelude::Mentionable;
 pub async fn ping(ctx: CommandContext<'_>) -> Result<()> {
     let channel_id = ctx.channel_id();
 
-    let mut thread_members = channel_id
+    let bot_id = ctx.discord().http.get_current_user().await?.id;
+    let author_id = ctx.author().id;
+
+    let thread_members = &channel_id
         .get_thread_members(&ctx.discord().http)
         .await
         .context("It seems like you are not in a thread, dear~")?;
 
-    thread_members.sort_by_key(|member| Reverse(member.join_timestamp.timestamp()));
-
     let oldest_member = thread_members
-        .pop()
+        .iter()
+        .min_by_key(|member| member.join_timestamp.timestamp())
         .context("It seems like there was no user here..? I'm confused")?;
 
-    if oldest_member
+    let owner_id = oldest_member
         .user_id
-        .and_then(|id| (id == ctx.author().id).into())
-        .is_some()
-    {
-        let mentions: Vec<String> = thread_members
-            .into_iter()
-            .filter_map(|member| member.user_id.and_then(|id| Some(id.mention().to_string())))
-            .collect();
+        .and_then(|id| if id == author_id { Some(id) } else { None })
+        .context("Ara, you shouldn't be doing this without permission~")?;
 
-        ctx.send(|m| {
-            m.content(format!(
-                "Calling forth everyone in this lovely thread!\n{}",
-                mentions.join(" ")
-            ))
-        })
-        .await?;
-    }
+    let mentions: Vec<String> = thread_members
+        .into_iter()
+        .filter_map(|member| member.user_id)
+        .filter(|id| *id != owner_id && *id != bot_id)
+        .map(|id| id.mention().to_string())
+        .collect();
+
+    ctx.send(|m| {
+        m.content(format!(
+            "Calling forth everyone in this lovely thread!\n{}",
+            mentions.join(" ")
+        ))
+    })
+    .await?;
 
     Ok(())
 }
