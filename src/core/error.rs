2aa5b1b35611ea42b509b372bee80cb9355b544a
Jannis Becker <jbecker@jannisbecker.me>
2022-06-23 18:08:56 +0200

    fix: minor fixes in error handler

--- src/core/error.rs
+++ src/core/error.rs
@@ -2,31 +2,30 @@ use poise::serenity_prelude::{Color, CreateEmbed};
 
 use crate::core::context::ContextData;
 
-pub fn error_embed(e: &mut CreateEmbed, err: anyhow::Error) -> &mut CreateEmbed {
+pub fn command_error_embed(e: &mut CreateEmbed, err: anyhow::Error) -> &mut CreateEmbed {
     e.color(Color::DARK_RED)
-        .title("Something happened")
+        .title("Ara?")
         .description(err.to_string())
 }
 
 pub async fn on_error(error: poise::FrameworkError<'_, ContextData, anyhow::Error>) {
     match error {
-        poise::FrameworkError::Setup { error } => {
-            tracing::error!(?error, "failed to setup bot")
-        }
-
         poise::FrameworkError::Command { error, ctx } => {
-            tracing::error!(command = ctx.command().name, ?error, "error in command");
+            tracing::error!(%error, "command error");
 
             let _ = ctx
-                .send(|m| m.embed(|embed| error_embed(embed, error)))
+                .send(|m| {
+                    m.embed(|embed| command_error_embed(embed, error))
+                        .ephemeral(true)
+                })
                 .await;
         }
 
         error => {
-            tracing::error!(?error, "calling default error handler");
+            tracing::error!("invoking default error handler");
 
             if let Err(e) = poise::builtins::on_error(error).await {
-                tracing::error!(?e, "error in default error handler")
+                tracing::error!(%e, "error in default error handler")
             }
         }
     }
